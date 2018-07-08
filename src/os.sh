9400fd9bc082e1cef54d507dfd970661074e7004
Purinda Gunasekara <purinda@gmail.com>
2018-07-08 23:28:45 +1000

    Fixed an issue with let playbook depend on pretasks for ubuntu distros. Moved distro specific python 2.x installation into a pre task which works on ubuntu or skip for non ubuntu distros.
    Added os.sh which is used for determining the os platform this playbook
    is being run.

---
+++ src/os.sh
@@ -0,0 +1,15 @@
+[ $CLI_OS ] && return || CLI_OS=1
+
+os_family=""
+
+function detect_os() {
+    if [ "$(uname)" == "Darwin" ]; then
+        os_family="OSX"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        os_family="Linux"
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        os_family="Windows"
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        os_family="Windows"
+    fi
+}
